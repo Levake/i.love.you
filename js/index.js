0264606fe4ba83cfa89b6b86117a3a2301ce7453
Levake <55313008+Levake@users.noreply.github.com>
2020-09-30 05:39:16 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -81,7 +81,32 @@ var TextScramble = function () {
 // Example
 // ——————————————————————————————————————————————————
 
-var phrases = ['Я,', 'люблю тебя.', 'Это всего лишь год', 'и не больше.', '365 дней,', 'мы справимся.', 'Правда?', 'Не плачь.','Тебе не идут слёзы,','помнишь?', 'Самое главной не забывай', 'меня и наши воспоминания.', 'Это наше всё...', '... на данный момент.','Если ты захочешь','мы всё преодолеем.','Ну а сейчас,','я там, ты тут.','И всё по новой.','Потому что...'];
+var phrases = ['Я 🙂'
+,'люблю тебя. ♥️'
+,'Это всего лишь год, 🕓'
+,'каких-то 365 дней 📅'
+,'мы справимся. 😚'
+,'Правда? 🤗'
+,'Не плачь. 😔'
+,'Тебе не идут слёзы, 😥'
+,'кроме слёз радости, 😂'
+,'помнишь? 🤭'
+,'Самое главной не забывай 🙃'
+,'меня и наши воспоминания. ✨'
+,'Это наше всё... 🥺'
+,'на данный момент. 🙄'
+,'И вот я вернусь 🚂'
+,'и начнём с чистого листа. 📝'
+,'Если ты захочешь. 🥰'
+,'Ну а сейчас, 😘'
+,'я там, ты тут. 😔'
+,'Вроде расстояние, 🌏'
+,'но всё равно я чувствую, 💕'
+,'тебя рядом. 😉'
+,'Ну, а это сообщение, 💌'
+,'будет повторяться, 🔄'
+,'бесконечно. 🔃'
+,'А всё потому что... ❣️'];
 
 var el = document.querySelector('.text');
 var fx = new TextScramble(el);
@@ -89,9 +114,28 @@ var fx = new TextScramble(el);
 var counter = 0;
 var next = function next() {
   fx.setText(phrases[counter]).then(function () {
-    setTimeout(next, 1500);
+    setTimeout(next, 2500);
   });
   counter = (counter + 1) % phrases.length;
 };
 
-next();
+var onclick = function onclick() 
+{
+  let button = document.getElementById("button");
+  button.style.display = 'none';
+  
+  let button4 = document.getElementById("button2");
+  button4.style.display = 'none';
+  
+  let button1 = document.getElementById("div1");
+  button1.style.display = 'flex';
+  
+  let button2 = document.getElementById("div2");
+  button2.style.display = 'flex';
+  
+  var audio = new Audio();
+  audio.src = 'misuc/music.mp3';
+  audio.play();
+  
+  next();
+}
